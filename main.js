7c1da8f297ce0ae5421b259b49fb233686286d53
Pranali777 <shirsat7pranali@gmail.com>
2022-02-26 20:32:20 +0100

    Added push method

--- main.js
+++ main.js
@@ -3,19 +3,36 @@ class StackDataStructure {
     size_of_stack;
 
     constructor() {
-        this.astack = [];
-        this.size = 5;
+        this.a_stack = [];
+        this.size_of_stack = 5;
+    }
+
+    push(num) {
+
+        if (this.a_stack.length < this.size_of_stack) {
+                    
+            this.a_stack[this.a_stack.length] = num;
+            console.log(this.a_stack);
+        }
+        else {
+            console.log("Cannot add more than 5 elements");
+        }
     }
 
 
     checksize(){
-        console.log(`The current size of the stack = ${this.astack.length}`);
+        console.log(`The current size of the stack = ${this.a_stack.length}`);
     }
 }
 
 
 let sds = new StackDataStructure();
 sds.checksize();
-
+sds.push(4);
+sds.push(7);
+sds.push(2);
+sds.push(5);
+sds.push(0);
+sds.push(4);
 
 
